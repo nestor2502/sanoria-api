5c813c7ecf7f2c4863fc38e71dfca9a904f8cc47
nestor2502 <nvazquez@techassist.mx>
2021-12-29 19:08:23 -0600

    change port

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  //await app.listen(3000);
-  await app.listen("https://sanoria-api.herokuapp.com/");
+  const PORT = process.env.PORT || 3000
+  await app.listen(PORT);
 }
 bootstrap();
