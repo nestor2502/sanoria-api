9972713f907bbd297efd6bda20bac0bf84d3bf12
nestor2502 <nvazquez@techassist.mx>
2022-01-06 21:47:14 -0600

    user crud

---
+++ src/user/user.controller.ts
@@ -0,0 +1,44 @@
+import { Controller, Get, Headers, Post, Body, Delete, Param, Patch } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('user')
+export class UserController {
+  
+  constructor(
+    private readonly userService: UserService,
+    ){
+  }
+
+  @Post()
+  create(@Body() user: CreateUserDto){
+    return this.userService.create(user);
+  }
+
+  @Get(':userId')
+  findOne(@Param('userId') userId: string){
+    return this.userService.findOne(userId);
+  }
+
+  @Patch(':userId')
+  update(@Param('userId') userId: string, @Body() user: UpdateUserDto){
+    return this.userService.update(userId, user);
+  }
+
+  @Delete(':userId')
+  remove(@Param('userId') userId: string){
+    return this.userService.remove(userId);
+  }
+
+  @Get('login')
+  login(@Headers('email') email: string, @Headers('password') password: string){
+    return this.userService.login(email, password);
+  }
+
+  @Delete('logout/:userId')
+  logout(@Param('userId') userId){
+    return this.userService.logout(userId);
+  }
+
+}
